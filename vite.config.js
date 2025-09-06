0094e35cb9eab55f3538876b58d6bb0dd5137dae
soberanosweetkylah-dot <soberano.sweetkylah@marsu.edu.ph>
2025-09-06 18:31:29 +0800

    Set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/soberanosweetkylah-dot/", // <--- Set this to your repo name
   plugins: [react()],
   server: {
     watch: {
