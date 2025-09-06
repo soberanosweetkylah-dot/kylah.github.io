12cacb675be8e1a7eba765dc8158a084662026c1
soberanosweetkylah-dot <soberano.sweetkylah@marsu.edu.ph>
2025-09-06 18:39:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/soberanosweetkylah-dot/", // <--- Set this to your repo name
+base: "/kylah.github.io/",
   plugins: [react()],
   server: {
     watch: {
